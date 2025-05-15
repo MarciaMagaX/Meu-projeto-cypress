61ea8deb70a893235de54d0588aebdb6cf0be3d7
Marcia Xavier <135170338+MarciaMagaX@users.noreply.github.com>
2025-05-15 18:57:34 -0300

    feat: mochawesome fix e checkout scenario

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,7 @@
 const { defineConfig } = require("cypress");
 require('dotenv').config(); // Carrega variáveis do .env
 
-module.exports = defineConfig({
-  reporter: "mochawesome", // Define o reporter
-  reporterOptions: {
-    reportDir: "cypress/reports/mochawesome", // Pasta onde salva os relatórios
-    reportFilename: "report-[datetime]",       // Nome do arquivo com data/hora
-    overwrite: false,                          // Não sobrescreve relatórios antigos
-    html: true,                                // Gera relatório em HTML
-    json: true,                                // Gera relatório em JSON
-    charts: true,
-    inlineAssets: true,
-  },
+module.exports = defineConfig({  
   e2e: {
     setupNodeEvents(on, config) {
       config.env.email = process.env.CYPRESS_EMAIL;
