26c187d18244c6c6ac2ba61631bb71a47a2c2b1d
Marcia Xavier <135170338+MarciaMagaX@users.noreply.github.com>
2025-05-24 20:46:18 -0300

    feat: retirar comments

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,23 +7,22 @@ describe('Funcionalidade de Login - SauceDemo', () => {
   })
 
   it('Deve fazer login com credenciais válidas', () => {
-    // cy.get('#user-name').should('be.visible').type('standard_user').should('have.value', 'standard_user')
     LoginPage.preencherUsername('standard_user')    
-    // cy.get('#password').should('be.visible').type('secret_sauce').should('have.value', 'secret_sauce')
+
     LoginPage.preencherPassword('secret_sauce') 
-    // cy.get('#login-button').should('be.visible').click()
+    
     LoginPage.btnLogar()
-    // cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+    
     LoginPage.acessarInventario()
   })
 
   it('Deve exibir erro ao inserir username inválido', () => {
     LoginPage.preencherUsername('usuario_falso')
-    // cy.get('#password').should('be.visible').type('secret_sauce')
+    
     LoginPage.preencherPassword('secret_sauce')
-    // cy.get('#login-button').should('be.visible').click()
+    
     LoginPage.btnLogar()
-    // cy.get('[data-test="error"]').should('be.visible').and('contain.text', 'Username and password do not match any user in this service')
+    
     LoginPage.verificarMensagemErro('Username and password do not match any user')
   })
 
